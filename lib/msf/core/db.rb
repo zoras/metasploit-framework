8e414a8bfab9641c81088d22f73033be5b37a700
Tod Beardsley <todb@metasploit.com>
2012-04-15 15:06:08 -0500

    Ruby, come on. Ducktype this. Please.
    Use interpolated strings to get the to_s behavior you don't get with
    just plussing.

--- lib/msf/core/db.rb
+++ lib/msf/core/db.rb
@@ -1236,10 +1236,10 @@ class DBManager
 		if opts[:refs]
 			rids = []
 			opts[:refs].each do |r|
-				if r.respond_to? :ctx_id
-					r = r.ctx_id + '-' + r.ctx_val
+				if (r.respond_to?(:ctx_id)) and (r.respond_to?(:ctx_val))
+					r = "#{r.ctx_id}-#{r.ctx_val}"
+					rids << find_or_create_ref(:name => r)
 				end
-				rids << find_or_create_ref(:name => r)
 			end
 		end
 
