bda6479d154cf75572dd5de8b66bfde661a55de9
HD Moore <hd_moore@rapid7.com>
2012-04-15 18:53:44 -0500

    Fix 1.8.x compatibility

--- lib/msf/env/gemcache.rb
+++ lib/msf/env/gemcache.rb
@@ -9,7 +9,7 @@ class Gemcache
 	@@msfbase = ::File.expand_path(::File.join(::File.dirname(__FILE__), '..', '..', '..'))
 	@@gembase = ::File.join(@@msfbase, "lib/gemcache")
 	@@gemarch = ( RUBY_PLATFORM =~ /mingw/ ? 'win32' : ( RUBY_PLATFORM =~ /x86_64.*linux/ ? 'linux64' : (RUBY_PLATFORM =~ /i\d86.*linux/ ? 'linux32' : 'unknown') ) )
-	@@rubvers =	RUBY_VERSION =~ /^(1\.9\.|2\.)/ ? '1.9.1' : RUBY_VERSION
+	@@rubvers =	'1.9.1'
 	
 	def self.configure
 		return if not ::File.exist?(@@gembase)
