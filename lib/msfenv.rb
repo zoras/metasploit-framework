34af04076a068e9f60c5526045ddbba5fca359fd
HD Moore <hd_moore@rapid7.com>
2012-04-15 18:18:29 -0500

    Fallback to bundler when not running inside of a installer env

--- lib/msfenv.rb
+++ lib/msfenv.rb
@@ -2,9 +2,12 @@
 # Shim load the bundled gem cache if the environment is set
 #
 
+_msf_gemcache = false
+
 # If the bundle option is explicitly set, load the gemcache
 if ENV['MSF_BUNDLE_GEMS'].to_s.downcase =~ /^[yt1]/
 	require 'msf/env/gemcache'
+	_msf_gemcache = true
 else
 	# If the bundle option is empty and this looks like an installer environment
 	# also load the gem cache (but probably not the binary gem cache)
@@ -12,7 +15,14 @@ else
 		::File.exists?( File.join( File.dirname(__FILE__), "..", "..", "properties.ini") ) and
 		::File.directory?( File.join( File.dirname(__FILE__), "..", "..", "apps", "pro") )
 			require 'msf/env/gemcache'
+			_msf_gemcache = true
 	end
 end
 
-	
+if not _msf_gemcache
+	# The user is running outside of the installer environment and not using
+	# our bundled gemset, so we fallback on bundler instead
+	ENV['BUNDLE_GEMFILE'] = ::File.expand_path(::File.join(::File.dirname(__FILE__), "..", "documentation", "Gemfile"))
+	ENV['BUNDLE_PATH']    = ENV['GEM_HOME']
+	require 'bundler/setup'
+end
