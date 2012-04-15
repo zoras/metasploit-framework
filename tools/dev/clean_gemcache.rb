ea6f3f6c434537ca15b6c6674e31081e27ce7f86
HD Moore <hd_moore@rapid7.com>
2012-04-15 14:54:42 -0500

    Cleanup uncessary .so files (ext vs lib)

--- tools/dev/clean_gemcache.rb
+++ tools/dev/clean_gemcache.rb
@@ -6,3 +6,11 @@ base = ::File.expand_path(::File.join(::File.dirname(__FILE__), "..", "..", "lib
 Dir.glob("#{base}/**/cache/*.gem").each do |gem|
 	::FileUtils.rm_rf(gem)
 end
+
+Dir.glob("#{base}/**/*.o").each do |obj|
+	::FileUtils.rm_rf(obj)
+end
+
+Dir.glob("#{base}/**/ext/*.so").each do |obj|
+	::FileUtils.rm_rf(obj)
+end
